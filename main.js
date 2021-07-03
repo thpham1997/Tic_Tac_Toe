2b6191fa11c63eb89cfd20da00d8e772384ceabc
TP1997 <thanhphamlike@gmail.com>
2021-07-02 23:44:52 -0400

    Finished logic for game, added individual display for controler, added score into player

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ const gameBoard = (() => {
   const PLAYGROUND = document.querySelector('.playground');
   let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
-  function _markSpot(e, player) {
-    console.log(!!e.target.getAttribute('data-taken'));
-    if (!!e.target.getAttribute('data-taken')) {
-      e.target.textContent = player.getChoice();
+  function _markSpot(e, players) {
+    console.log(e.target.getAttribute('data-taken') === 'true');
+    if (e.target.getAttribute('data-taken') === 'false') {
+      e.target.textContent = players[0].getTurn() ? players[0].getChoice() : players[1].getChoice();
+      board[e.target.getAttribute('data-index')] = e.target.textContent;
       e.target.setAttribute('data-taken', true);
     }
+    console.log(board);
   }
-  function display() {
+  function getBoard() {
+    return board;
+  }
+  function display(controler, players) {
     for (let i = 0; i < board.length; i++) {
       let cellDiv = document.createElement('div');
       cellDiv.addEventListener('click', (e) => {
         console.log('event run');
-        _markSpot(e, player1);
+        _markSpot(e, players);
+        controler.switchTurn(players[0], players[1]);
+        controler.setResult(controler.checkGame(board));
+        console.log(controler.getResult());
       });
       cellDiv.classList.add('cell');
       cellDiv.textContent = board[i];
@@ -31,26 +39,84 @@ const gameBoard = (() => {
 })();
 
 // controler object
-const displayControler = (() => {
+const controler = (() => {
+  const PLAYER_SCORE = document.getElementsByClassName('control-panel__score__player');
+  const COMP_SCORE = document.getElementsByClassName('control-panel__score__comp');
 
-  return {};
+  function checkGame(board) {
+    // vertical and horizontal check 'X'
+    if (board[0] === board[1] && board[0] === board[2] && board[0] === 'X') return 'X';
+    else if (board[0] === board[3] && board[0] === board[6] && board[0] === 'X') return 'X';
+    else if (board[1] === board[4] && board[1] === board[7] && board[1] === 'X') return 'X';
+    else if (board[2] === board[5] && board[2] === board[8] && board[2] === 'X') return 'X';
+    else if (board[3] === board[4] && board[3] === board[5] && board[3] === 'X') return 'X';
+    else if (board[6] === board[7] && board[6] === board[8] && board[6] === 'X') return 'X';
+
+    // vertical and horizontal check '0'
+    else if (board[0] === board[1] && board[0] === board[2] && board[0] === 'O') return 'O';
+    else if (board[0] === board[3] && board[0] === board[6] && board[0] === 'O') return 'O';
+    else if (board[1] === board[4] && board[1] === board[7] && board[1] === 'O') return 'O';
+    else if (board[2] === board[5] && board[2] === board[8] && board[2] === 'O') return 'O';
+    else if (board[3] === board[4] && board[3] === board[5] && board[3] === 'O') return 'O';
+    else if (board[6] === board[7] && board[6] === board[8] && board[6] === 'O') return 'O';
+
+    // tie 
+    else if (board.every(element => element !== ' ')) return 'tie';
+
+    // not finisd and no one wins
+
+    else return 'undone';
+  }
+
+  function setResult(result) {
+    this.result = result
+  }
+  function getResult() {
+    return this.result;
+  }
+
+  function switchTurn(player1, player2) {
+    player1.setTurn(!player1.getTurn());
+    player2.setTurn(!player2.getTurn());
+  }
+
+  function display(players) {
+    console.log('run');
+    PLAYER_SCORE.textContent = players[0].getScore();
+    COMP_SCORE.textContent = players[1].getScore();
+  }
+  return { display, switchTurn, setResult, getResult, checkGame };
 })();
 
 // player object
-const player = (name, isPlayer) => {
-  this.isPlayer = isPlayer;
-  this.choice = '';
+const player = (name) => {
   function setChoice(choice) {
     this.choice = choice;
   }
   function getChoice() {
     return this.choice;
   }
-  function isPlayer() {
-    return this.isPlayer;
+  function setTurn(turn) {
+    this.turn = turn;
   }
-  return { isPlayer, setChoice, getChoice };
+  function getTurn() {
+    return this.turn;
+  }
+  function setScore(score) {
+    this.score = score;
+  }
+  function getScore() {
+    return this.score;
+  }
+  return { setChoice, getChoice, setTurn, getTurn, setScore, getScore };
 }
-player1 = player('thanh', true);
+let player1 = player('thanh');
 player1.setChoice('X');
-gameBoard.display();
+player1.setTurn(true);
+player1.setScore(0);
+let player2 = player('comp');
+player2.setChoice('O');
+player2.setTurn(false);
+player2.setScore(0);
+gameBoard.display(controler, [player1, player2]);
+controler.display([player1, player2]);
